4fb597ad3cfaab3ed908bf73459ca42ada4db2ba
sungminyun1 <smy07207@gmail.com>
2023-03-28 22:53:13 +0900

    03.28

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         // 이거 예외는 기존 테스트코드 사용 피하기위해. 안해주면 기존 appConfig나 testConfig도 다 물려버림
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class),
+        basePackages = "hello.core.member"
 )
 public class AutoAppConfig {
 }
